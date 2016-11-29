1c0cb311e82fd836d970e06d64550b419251d91c
AlexanderKonovalov <konoaleksandr@mail.ru>
2016-11-29 00:04:05 -0800

    validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+validates :title, uniqueness: true
+validates:description , presence:true
+validates:image_url, allow_blank:true, format: {with: %r{\.(gif|jpg|png)\Z}i,message: 'URLneedforamtgif,jpg,png' }
+validates:price, numericality:{greater_than_or_equal_to: 0.01}
 end
